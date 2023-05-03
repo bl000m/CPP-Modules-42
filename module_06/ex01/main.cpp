2306d9ad6538dd2fce636ed76f7c0a511cc82589
bl000m <mathiapagani@gmail.com>
2023-05-03 12:11:56 +0200

    ok

--- module_06/ex01/main.cpp
+++ module_06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mpagani <mpagani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 13:25:23 by mpagani           #+#    #+#             */
-/*   Updated: 2023/05/02 13:08:48 by mpagani          ###   ########.fr       */
+/*   Updated: 2023/05/03 12:06:14 by mpagani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ int	main(void){
 	data.age = 30;
 
 	std::cout << "--------- Displaying data members before serializing + deserializing " << std::endl;
-	std::cout << "Name: " << data.name << " Age: " << data.age << std::endl;
+	std::cout << "Name: " << data.name << "- Age: " << data.age << std::endl;
 	uintptr_t ptr = Serializer::serialize(&data);
 	Data *dataDeserialized = Serializer::deserialize(ptr);
 	std::cout << "--------- Displaying data members after serializing + deserializing " << std::endl;
-	std::cout << "Name: " << dataDeserialized->name << " Age: " << dataDeserialized->age << std::endl;
+	std::cout << "Name: " << dataDeserialized->name << "- Age: " << dataDeserialized->age << std::endl;
 	return 0;
 }
--- module_06/ex02/main.cpp
+++ module_06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mpagani <mpagani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 13:25:23 by mpagani           #+#    #+#             */
-/*   Updated: 2023/05/02 16:53:36 by mpagani          ###   ########.fr       */
+/*   Updated: 2023/05/03 12:10:07 by mpagani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ void identify(Base* p){
 	else if (dynamic_cast<B*>(p))
 		std::cout << "Class identified starting from a pointer: B" << std::endl;
 	else if (dynamic_cast<C*>(p))
-		std::cout << "Class identified: starting from a pointer C" << std::endl;
+		std::cout << "Class identified starting from a pointer: C" << std::endl;
 	else
 		std::cout << "Class not identified starting from a pointer" << std::endl;
 
@@ -75,7 +75,7 @@ int	main(void){
 		delete randomGenerate;
 	}
 	catch (std::bad_alloc & e){
-		std::cout << "Something wrong happemed in allocation memory" << e.what() << std::endl;
+		std::cout << "Something wrong happened" << e.what() << std::endl;
 		return 1;
 	}
 	return 0;
