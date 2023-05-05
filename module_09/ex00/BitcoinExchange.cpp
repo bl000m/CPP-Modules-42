b656cbc57a107a499f1fa2862aeb449da98206d3
bl000m <mathiapagani@gmail.com>
2023-05-05 17:20:16 +0200

    setting 09

--- module_09/ex00/BitcoinExchange.cpp
+++ module_09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,24 @@
 /*   By: mpagani <mpagani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 14:58:44 by mpagani           #+#    #+#             */
-/*   Updated: 2023/05/05 14:58:45 by mpagani          ###   ########.fr       */
+/*   Updated: 2023/05/05 16:21:17 by mpagani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange(){}
+
+BitcoinExchange::~BitcoinExchange(){}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &src){
+	*this = src;
+}
+
+BitcoinExchange & BitcoinExchange::operator=(const BitcoinExchange &rhs){
+	if (&rhs != this)
+	*this = rhs;
+	return *this;
+}
+
 
--- module_09/ex00/main.cpp
+++ module_09/ex00/main.cpp
@@ -6,7 +6,28 @@
 /*   By: mpagani <mpagani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 14:59:06 by mpagani           #+#    #+#             */
-/*   Updated: 2023/05/05 14:59:07 by mpagani          ###   ########.fr       */
+/*   Updated: 2023/05/05 16:55:54 by mpagani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "BitcoinExchange.hpp"
+#include <map>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+int main(){
+	std::map<std::string, int> recordToCheck;
+	std::string file_name("input.txt");
+	std::ifstream file(file_name.c_str());
+	if (!file.is_open()){
+		std::perror(file_name.c_str());
+		return 1;
+	}
+	std::string line;
+	std::getline(file, line);
+	parse(line); 
+	file.close();
+	return 0;
+}
