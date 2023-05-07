b0b366a3ef21074ff0b5628978bbe25c2921627a
bl000m <mathiapagani@gmail.com>
2023-05-07 06:47:21 +0000

    parsing

--- module_09/ex00/main.cpp
+++ module_09/ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mpagani <mpagani@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mathia <mathia@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 14:59:06 by mpagani           #+#    #+#             */
-/*   Updated: 2023/05/05 16:55:54 by mpagani          ###   ########.fr       */
+/*   Updated: 2023/05/07 06:47:07 by mathia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@
 #include <fstream>
 #include <string>
 
+void parse(std::map<std::string, int>, std::string line){
+	
+}
 
 int main(){
 	std::map<std::string, int> recordToCheck;
@@ -27,7 +30,7 @@ int main(){
 	}
 	std::string line;
 	std::getline(file, line);
-	parse(line); 
+	parse(recordToCheck, line); 
 	file.close();
 	return 0;
 }
