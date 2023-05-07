bf2c9cc8de1e8403c81cd078b3f92979f8a2a47d
bl000m <mathiapagani@gmail.com>
2023-05-07 09:14:19 +0000

    ex00 compiling well but still to be implemented the research value func

--- module_09/ex00/main.cpp
+++ module_09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mathia <mathia@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 14:59:06 by mpagani           #+#    #+#             */
-/*   Updated: 2023/05/07 06:47:07 by mathia           ###   ########.fr       */
+/*   Updated: 2023/05/07 09:13:42 by mathia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,42 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 
-void parse(std::map<std::string, int>, std::string line){
-	
+int checkError(int numArgs){
+	if (numArgs != 2){
+		std::cerr << "Error: please type ./btc <input file name (file containing bitcoin to be converted in dollar depending on the related date)>" << std::endl;
+		return 1;
+	}
+	return 0;
 }
 
-int main(){
+void parse(std::map<std::string, int> recordToCheck, std::istream &file, std::string &line){
+	while (std::getline(file, line)){
+		std::stringstream content(line);
+		std::string date;
+		float bitcoinQty;
+		if (std::getline(content, date, '|') && content >> bitcoinQty)
+		{
+			std::cout << "date:" << date << std::endl;
+			std::cout << "bitcoinQty:" << bitcoinQty << std::endl;
+			recordToCheck.insert(std::make_pair(date, bitcoinQty));
+		}
+	}
+}
+
+int main(int argc, char **argv){
+	if (checkError(argc))
+		return 1;
 	std::map<std::string, int> recordToCheck;
-	std::string file_name("input.txt");
-	std::ifstream file(file_name.c_str());
+	std::ifstream file(argv[1]);
 	if (!file.is_open()){
-		std::perror(file_name.c_str());
+		std::perror(argv[1]);
 		return 1;
 	}
 	std::string line;
-	std::getline(file, line);
-	parse(recordToCheck, line); 
+	std::getline(file, line); // first getline to jump the first line
+	parse(recordToCheck, file, line); 
 	file.close();
 	return 0;
 }
