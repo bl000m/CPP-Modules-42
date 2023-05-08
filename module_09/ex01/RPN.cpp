3ce6bf5d80eaf09a20d9410ee2e9acce63d9fba3
bl000m <mathiapagani@gmail.com>
2023-05-08 16:12:08 +0200

    ex01 ok

--- module_09/ex01/RPN.cpp
+++ module_09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: mpagani <mpagani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 14:16:29 by mpagani           #+#    #+#             */
-/*   Updated: 2023/05/08 15:16:40 by mpagani          ###   ########.fr       */
+/*   Updated: 2023/05/08 16:11:23 by mpagani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,42 +34,49 @@ float RPN::getResult(float operand1, float operand2, char oper){
 	if (oper == '+')
 		return operand1 + operand2;
 	if (oper == '-')
-		return operand1 - operand2;
+		return operand2 - operand1;
 	if (oper == '*')
 		return operand1 * operand2;
 	if (oper == '/'){
-		if (operand2 == 0)
+		if (operand1 == 0)
 			throw SomethingWrong("Error: cannot divide by 0");
-		return operand1 / operand2;
+		return operand2 / operand1;
 	}
-	return 0;
+	else
+        throw SomethingWrong("Error: invalid operator");
 }
 
 float RPN::givingTheResult(std::string expression){
 	std::string operand;
 	float operand1 = 0;
 	float operand2 = 0;
+	float result = 0;
 	std::stack<float> operands;
 
 	for (size_t i = 0; i < expression.length(); i++){
+		if (expression[i] == ' ')
+			continue;
 		if (isdigit(expression[i]))
 			operand += expression[i];
 		else if (isOperator(expression[i])){
-			if (operands.size() != 2)
+			if (operands.size() < 2)
 				throw SomethingWrong("Error: not enough operand to execute a RPN expression");
 			operand1 = operands.top();
 			operands.pop();
 			operand2 = operands.top();
 			operands.pop();
-			operands.push(getResult(operand1, operand2, expression[i]));
+			result = getResult(operand1, operand2, expression[i]);
+			operands.push(result);
 		}
 		else
-			throw SomethingWrong("Error: invalid token");
+				throw SomethingWrong("Error: invalid token");
 		if (operand.empty() == 0){
-			operands.push(atoi(operand.c_str()));
+			operands.push(atof(operand.c_str()));
 			operand.clear();
 		}
-
 	}
+	if (operands.size() != 1)
+		throw SomethingWrong("Error: too many operands");
+
 	return operands.top();
 }
